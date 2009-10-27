b6f35179eb8eedf10d61c780925a9444a2b1304f
bhejblum <bhejblum@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-10-27 17:39:23 +0000

    classe Lieu, methodes ajouterVelo et enleverVelo (partielle)

--- ProjetInfo/src/metier/Lieu.java
+++ ProjetInfo/src/metier/Lieu.java
@@ -1,24 +1,32 @@
 package metier;
 
+import gestionBaseDeDonnees.ConnexionOracleViaJdbc;
+
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public abstract class Lieu {
 
 	//Attributs
-	
+
 	private String id;
 	private int type;
 	private String adresse;
 	private int capacite;
 
-	
+
 	//Constantes
-	
+
 	public static final int TYPE_STATION=1;
 	public static final int TYPE_GARAGE=2;
 	public static final int CAPACITE_GARAGE=1000;
-	
+
+	//pas de constructeur puisqu'il s'agit d'une classe abstraite
+
 	
 	//Accesseurs
-	
+
 	public String getId() {
 		return id;
 	}
@@ -43,17 +51,37 @@ public abstract class Lieu {
 	public void setCapacite(int capacite) {
 		this.capacite = capacite;
 	}
-	
-	
-	//Methodes
-	
 
-	public void enleverVelo(Velo velo){
-		
+
+	//Methodes
+
+
+	public void enleverVelo(Velo velo) throws SQLException, ClassNotFoundException{
+
+		ConnexionOracleViaJdbc.ouvrir();
+		Statement s = ConnexionOracleViaJdbc.createStatement();
+		/*TODO
+		 * s.executeUpdate(//requete SQL ˆ ecrire);
+		 */
+		ConnexionOracleViaJdbc.fermer();
+		/*TODO
+		 * gerer les exceptions
+		 */
+
 	}
-	
-	public void ajouterVelo(Velo velo){
-		
+
+	public void ajouterVelo(Velo velo) throws SQLException, ClassNotFoundException{
+		try{
+			ConnexionOracleViaJdbc.ouvrir();
+			Statement s = ConnexionOracleViaJdbc.createStatement();
+			s.executeUpdate("INSERT into Velo values ('" 
+					+ velo.getId() + "', '"+ velo.getLieu().getId() + "')");
+			ConnexionOracleViaJdbc.fermer();
+		}
+		catch (SQLException e){
+			ConnexionOracleViaJdbc.fermer();//pour se deconnecter de la bdd meme si la requete sql souleve une exception
+		}
+
 	}
 
 }
