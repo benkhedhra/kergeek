e7ec9b7155f4d081f052512950e5010c7f1f4ba8
marine.dufournet@gmail.com <marine.dufournet@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-10-30 15:48:19 +0000

    Marine : création des tables BDD

--- trunk/ProjetInfo/src/gestionBaseDeDonnees/CreationTables.java
+++ trunk/ProjetInfo/src/gestionBaseDeDonnees/CreationTables.java
@@ -23,108 +23,54 @@ public class CreationTables {
 
 
 		/*TODO
-		 * Ci après, un exemple de creation de table (celui du tp de complemetn de java)
-		 * (a faire des que le modele de bdd en 3e forme normale est pret)
-		 *  
-
-		 Creation des tables:
-
-
+		 Marine (30/11) : CrÈations de tables ok, je les laisse en commentaires
+		 car j'ai des questions ‡ poser qui impliquerait modif
 
 
 		s.executeUpdate(
-				"CREATE TABLE TypeVoie (idTypeVoie CHAR(1)," +
-				"typeVoie VARCHAR2(20) NOT NULL, libelle VARCHAR2(30)," +
-				"CONSTRAINT pk_TypeVoie  PRIMARY KEY(idTypeVoie))"
-		);
-
-
+				"CREATE TABLE Utilisateur (idCompte number(4),	"+
+				"motDePasse varchar2(20),"+
+				"nom char(20) NOT NULL,"+
+				"prenom char(20),"+
+				"adressePostale varchar2(250),"+
+				"adresseMail varchar2(30),"+
+				"CONSTRAINT pk_Utilisateur  PRIMARY KEY(idCompte)");
 		s.executeUpdate(
-				"CREATE TABLE TypeNoeud (idTypeNoeud CHAR(1),"+
-				"typeNoeud VARCHAR2(10) NOT NULL," +
-				"CONSTRAINT pk_TypeNoeud  PRIMARY KEY(idTypeNoeud))"
-		);
-
+				"CREATE TABLE Lieu (idLieu number(4),	"+
+				"adresseLieu varchar2(250),"+
+				"capacite number(3),"+
+				"CONSTRAINT pk_Lieu  PRIMARY KEY(idLieu)");
 		s.executeUpdate(
-				"CREATE TABLE CategorieTrain (idCategorieTrain CHAR(1)," +
-				"categorie VARCHAR2(10) NOT NULL," +
-				"CONSTRAINT pk_CategorieTrain  PRIMARY KEY(idCategorieTrain))"
-		);
-
+				"CREATE TABLE Velo (idVelo number(4),	"+
+				"CONSTRAINT pk_Velo  PRIMARY KEY(idVelo),"+
+				"CONSTRAINT fk_Velo_Lieu FOREIGN KEY(idLieu) REFERENCES Lieu)");
+		
 		s.executeUpdate(
-				"CREATE TABLE Train (idTrain VARCHAR2(6)," + 
-				"nom VARCHAR2(5), idCategorieTrain CHAR(1) NOT NULL," +
-				"CONSTRAINT sk1_Train CHECK(nom IS NOT NULL),CONSTRAINT sk2_Train UNIQUE(nom)," +
-				"CONSTRAINT pk_Train  PRIMARY KEY(idTrain)," +
-				"CONSTRAINT fk_Train_CategorieTrain FOREIGN KEY(idCategorieTrain) REFERENCES CategorieTrain)" 
-		);
+				"CREATE TABLE Intervention (idInterventionntervention number(10)," +
+				"dateIntervention date NOT NULL," +
+				"typeIntervention NOT NULL, "+
+				"CONSTRAINT pk_Intervention  PRIMARY KEY(idIntervention),"+
+				"CONSTRAINT fk_Intervention_Velo FOREIGN KEY(idVelo) REFERENCES Velo)");
 		s.executeUpdate(
-				"CREATE TABLE Noeud (idNoeud VARCHAR2(6)," +
-				"nom VARCHAR2(20), idTypeNoeud CHAR(1)," +
-				"CONSTRAINT pk_Noeud PRIMARY KEY(idNoeud)," +
-				"CONSTRAINT sk1_Noeud CHECK(nom IS NOT NULL),CONSTRAINT sk2_Noeud UNIQUE(nom)," +
-				"CONSTRAINT fk_Noeud_TypeNoeud FOREIGN KEY(idTypeNoeud) REFERENCES TypeNoeud)"
-		);
-		s.executeUpdate(
-				"CREATE TABLE Troncon (idTroncon VARCHAR2(6)," + 
-				"idNoeud1 VARCHAR2(6), idNoeud2 VARCHAR2(6), distance INTEGER," + 
-				"CONSTRAINT pk_Troncon  PRIMARY KEY(idTroncon)," +
-				"CONSTRAINT fk_Troncon_Noeud1 FOREIGN KEY(idNoeud1) REFERENCES Noeud," +
-				"CONSTRAINT fk_Troncon_Noeud2 FOREIGN KEY(idNoeud2) REFERENCES Noeud)"
-		);
-		s.executeUpdate(
-				"CREATE TABLE Voie (idVoie VARCHAR2(8)," + 
-				"idTypeVoie CHAR(1), idTroncon VARCHAR2(6)," + 
-				"tempsSuccessionMinimal INTEGER," + 
-				"CONSTRAINT pk_Voie  PRIMARY KEY(idVoie)," +
-				"CONSTRAINT fk_Voie_TypeVoie FOREIGN KEY(idTypeVoie) REFERENCES TypeVoie," + 
-				"CONSTRAINT fk_Voie_Troncon FOREIGN KEY(idTroncon) REFERENCES Troncon)"		
-		);
-		s.executeUpdate(
-				"CREATE TABLE TempsParcours (idVoie VARCHAR2(8)," +
-				"idTrain VARCHAR2(6)," +
-				"tempsParcoursMin INTEGER,tempsParcoursMax INTEGER," +
-				"CONSTRAINT pk_TempsParcours  PRIMARY KEY(idVoie,idTrain)," +
-				"CONSTRAINT fk_TempsParcours_Voie FOREIGN KEY(idVoie) REFERENCES Voie," +
-				"CONSTRAINT fk_TempsParcours_Train FOREIGN KEY(idTrain) REFERENCES Train)"
-		);
-		s.executeUpdate(
-				"CREATE TABLE Passage (idPassage VARCHAR2(10)," + 
-				"idNoeudPassage VARCHAR2(6), idTrain VARCHAR2(6)," +
-				"idNoeudSuivant VARCHAR2(6), idVoie VARCHAR2(8), heureArrNormal DATE," +
-				"heureDepNormal DATE, dureeMin INTEGER, dureeMax INTEGER, heureArrMin DATE," + 
-				"heureArrMax DATE, heureDepMin DATE, heureDepMax DATE," +
-				"CONSTRAINT pk_Passage  PRIMARY KEY(idPassage)," +
-				"CONSTRAINT fk_Passage_NoeudPassage FOREIGN KEY(idNoeudPassage) REFERENCES Noeud," +
-				"CONSTRAINT fk_Passage_NoeudSuivant FOREIGN KEY(idNoeudSuivant) REFERENCES Noeud," +
-				"CONSTRAINT fk_Passage_Train FOREIGN KEY(idTrain) REFERENCES Train," +
-				"CONSTRAINT fk_Passage_Voie FOREIGN KEY(idVoie) REFERENCES Voie)"
-		);
-		s.executeUpdate(
-				"CREATE SEQUENCE ClefPassage MINVALUE 1 MAXVALUE 999999999"+ 
-				"START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE"		
-		);
-
+				"CREATE TABLE Empunt (idEmprunt number(10)," +
+				"dateEmprunt date NOT NULL," +
+				"dateRetour date NOT NULL, "+
+				"lieuEmprunt varchar2(250) NOT NULL," +
+				"lieuRetour varchar2(250) NOT NULL, "+
+				"CONSTRAINT pk_Empunt  PRIMARY KEY(idEmprunt),"+
+				"CONSTRAINT fk_Emprunt_Utilisateur FOREIGN KEY(idCompte) REFERENCES Utilisateur," +
+				"CONSTRAINT fk_Emprunt_Velo FOREIGN KEY(idVelo) REFERENCES Velo," +
+				"CONSTRAINT fk_Emprunt_Lieu FOREIGN KEY(idLieu) REFERENCES Lieu)");
 		s.executeUpdate("COMMIT"); // ne pas oublie le commit!
 		ConnexionOracleViaJdbc.fermer(); // et ne pas oublie non plus de fermer la base
 		System.out.println("Base cree.");*/
 
 
 
-
-
-
-
 		/* TODO
-		 * Ci après, un exemple de creation de table (celui du tp de complemetn de java)
-		 * 
+		 
 		
-		Update:
-		
-		
-		
-		
-		s.executeUpdate("insert into TypeVoie values('1','LGV','ligne ‡ grande vitesse')");
+		s.executeUpdate("insert into Utilisateur values('1000','kangourou','ligne ‡ grande vitesse')");
 		s.executeUpdate("insert into TypeVoie values('2','LE25','ligne ÈlectrifiÈe 25 000 VA')");
 		s.executeUpdate("insert into TypeVoie values('3','LE1.5','ligne ÈlectrifiÈe 1 500 VC')");
 		s.executeUpdate("insert into TypeVoie values('4','NE','ligne non ÈlectrifiÈe')");
