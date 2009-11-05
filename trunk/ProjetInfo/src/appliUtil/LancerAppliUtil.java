2cf9dc7bb028d90f81466658f8ebe351506a8891
maelle.font@gmail.com <maelle.font@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-11-05 14:52:31 +0000

    création de FenetreEmprunterVelo et de Panneau (seules classes à regarder pour l'instant), package ressources avec 2 images ajoutées

--- trunk/ProjetInfo/src/appliUtil/LancerAppliUtil.java
+++ trunk/ProjetInfo/src/appliUtil/LancerAppliUtil.java
@@ -4,8 +4,7 @@ public class LancerAppliUtil {
 
 	public static void main(String[] args) {
 
-		AccueilUtil f = new AccueilUtil("authentification");
-		f.setVisible(true);
+		FenetreEmprunterVelo f = new FenetreEmprunterVelo();
 
 	}
 }
