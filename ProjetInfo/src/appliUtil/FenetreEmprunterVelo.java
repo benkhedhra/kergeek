cb86e0f57e4cc75081193b77249cc47f6233c930
maelle.font <maelle.font@gmail.com>
2009-11-05 14:52:31 +0000

    création de FenetreEmprunterVelo et de Panneau (seules classes à regarder pour l'instant), package ressources avec 2 images ajoutées

--- ProjetInfo/src/appliUtil/FenetreEmprunterVelo.java
+++ ProjetInfo/src/appliUtil/FenetreEmprunterVelo.java
@@ -1,5 +1,40 @@
 package appliUtil;
 
-public class FenetreEmprunterVelo {
+import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FenetreEmprunterVelo extends JFrame {
+
+	public FenetreEmprunterVelo () {
+
+		//Définit un titre pour votre fenêtre
+		this.setTitle("Emprunter un vélo");
+		//Définit une taille pour celle-ci ; ici, 400 px de large et 500 px de haut
+		this.setSize(700, 500);
+		//Nous allons maintenant dire à notre objet de se positionner au centre
+		this.setLocationRelativeTo(null);
+		//Terminer le processus lorsqu'on clique sur "Fermer"
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//pour que la fenêtre ne se redimensionne pas à chaque fois
+		this.setResizable(false);
+		//pour que la fenêtre soit toujours au premier plan
+		this.setAlwaysOnTop(true);
+	/* les lignes suivantes servent à instancier un objet Jpanel et à définir sa couleur de fond
+	 * 
+	 * JPanel pan = new JPanel();
+	 * pan.setBackground(Color.BLUE);
+	 * */
+
+        this.setContentPane(new Panneau());
+        JButton bouton = new JButton("Bonjour bla bli blou");
+        this.getContentPane().add(bouton);    
+		
+		
+		
+		this.setVisible(true);
+	}       
 }
---
+++ ProjetInfo/src/appliUtil/Panneau.java
@@ -0,0 +1,34 @@
+package appliUtil;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Panneau extends JPanel {
+
+	public void paintComponent(Graphics g){
+
+		/*
+		 * ces deux lignes permettent de récupérer les largeurs et longueurs de l'écran
+		 * int x1 = this.getWidth();
+		 * int y1 = this.getHeight();
+		 * */
+
+		try {
+			Image img = ImageIO.read(new File(/*"ProjetInfo\\src\\ressources\\velo1.jpg"*/"C:\\Users\\Maëlle\\workspace\\ProjetInfo\\src\\ressources\\velo1.jpg"));
+			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		g.drawString("Vous êtes connecté en tant que Bli Blablou",15,30);
+		g.drawRoundRect(10, 10, 245, 30, 10, 10);
+	} 
+
+}
