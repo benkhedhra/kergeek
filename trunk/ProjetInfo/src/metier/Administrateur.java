1880208c9814747fa21ee22916a4a75004608999
bhejblum <bhejblum@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-11-06 13:27:24 +0000

    ajout du nombre de velos a bouger dans la dde d'assignation.

--- trunk/ProjetInfo/src/metier/Administrateur.java
+++ trunk/ProjetInfo/src/metier/Administrateur.java
@@ -13,17 +13,17 @@ import java.util.Date;
 
 public class Administrateur {
 
-	
-	
-	
+
+
+
 	// Attributs
 
 	private Compte compte;
 
-	
-	
-	
-	
+
+
+
+
 	// Constructeur
 
 	public Administrateur() {
@@ -31,11 +31,11 @@ public class Administrateur {
 		compte.setType(Compte.TYPE_ADMINISTRATEUR);
 	}
 
-	
-	
-	
-	
-	
+
+
+
+
+
 	// Accesseurs
 
 	public Compte getCompte() {
@@ -48,9 +48,9 @@ public class Administrateur {
 
 
 
-	
-	
-	
+
+
+
 	// Methodes
 
 	public Compte creerCompte() {
@@ -64,51 +64,51 @@ public class Administrateur {
 
 	}
 
-	
-	
-	
+
+
+
 	public void resilierCompte(String idCompte) throws SQLException,
 	ClassNotFoundException {
 
 		DAOCompte.getCompteById(idCompte).setActif(false);
 	}
 
-	
-	
+
+
 	// editerCompte() correspond à l'ensemble des setters!
 
-	
-	
-	
-	public void demanderAssignation(boolean ajout, Lieu lieu) throws SQLException, ClassNotFoundException, PasDansLaBaseDeDonneeException{
+
+
+
+	public void demanderAssignation(boolean ajout, int nombre, Lieu lieu) throws SQLException, ClassNotFoundException, PasDansLaBaseDeDonneeException{
 
 		Date dateCourante = UtilitaireDate.dateCourante();
-		
+
 		ConnexionOracleViaJdbc.ouvrir();
 		//ouverture de la connection à la bdd
-		
+
 		Statement s = ConnexionOracleViaJdbc.createStatement();
 		ResultSet res = s.executeQuery("Select idLieu from Lieu Where idLieu ='" + lieu.getId()+"'");
-		
+
 		/*verification de la prÈsence du lieu en question dans la base de donnees.
 		 * A terme, l'administrateur demandera une assignation alors qu'il s'interesse deja
 		 * a une station particuliere, cette verification sera alors superflue.
 		 */
-		
+
 		if (res.next()){
-		DemandeAssignation  ddeAssignation = new DemandeAssignation(dateCourante, ajout, lieu);
-		//creation de la demande d'assignation
+			DemandeAssignation  ddeAssignation = new DemandeAssignation(dateCourante, ajout, nombre, lieu);
+			//creation de la demande d'assignation
 
 
-		DAODemandeAssignation.entrerDemandeAssignation(ddeAssignation);
-		//entre la demande d'assignation dans la base de données
+			DAODemandeAssignation.entrerDemandeAssignation(ddeAssignation);
+			//entre la demande d'assignation dans la base de données
 		}
-		
+
 		else{
 			System.out.println("La station en question n'existe pas.");
 			throw new PasDansLaBaseDeDonneeException();
 		}
-		
+
 		ConnexionOracleViaJdbc.fermer();
 	}
 }
