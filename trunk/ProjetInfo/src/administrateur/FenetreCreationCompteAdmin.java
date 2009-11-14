7c220be0186edc860e7af15738d2f497d53e59fd
maelle.font@gmail.com <maelle.font@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-11-14 00:31:25 +0000

    résolution des pbs graphiques et poursuite de l'appliAdminTech : fenetre d'authentification ok, menu principal de l'admin ok conflits avec nicolas sur FenetreEmprunterVelo (et LancerAppliUtil) : à régler

--- trunk/ProjetInfo/src/administrateur/FenetreCreationCompteAdmin.java
+++ trunk/ProjetInfo/src/administrateur/FenetreCreationCompteAdmin.java
@@ -1,9 +1,5 @@
 package administrateur;
 
 public class FenetreCreationCompteAdmin {		
-	/* les lignes suivantes servent à instancier un objet Jpanel et à définir sa couleur de fond
-	 * 
-	 * JPanel pan = new JPanel();
-	 * pan.setBackground(Color.BLUE);
-	 * */
+
 }
--- trunk/ProjetInfo/src/administrateur/MenuPrincipalAdmin.java
+++ trunk/ProjetInfo/src/administrateur/MenuPrincipalAdmin.java
@@ -12,13 +12,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import metier.Administrateur;
+import metier.Compte;
 
-import org.jfree.chart.JFreeChart;
-
-import appliAdminTech.FenetreAuthentification;
+import appliUtil.FenetreAuthentificationUtil;
 
 public class MenuPrincipalAdmin extends JFrame implements ActionListener {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Administrateur admin;
 	private JLabel labelAdmin = new JLabel("");
 	private JButton boutonDeconnexion = new JButton("Déconnexion");
@@ -36,42 +39,57 @@ public class MenuPrincipalAdmin extends JFrame implements ActionListener {
 
 	public MenuPrincipalAdmin(Administrateur a){
 
-		this.setAdministrateur(a);
-		
-		System.out.println("Affichage du menu principal de l'administrateur");
-		//Définit un titre pour votre fenêtre
-		this.setTitle("Menu principal de l'administrateur");
-		//Définit une taille pour celle-ci ; ici, 400 px de large et 500 px de haut
-		this.setSize(700, 500);
-		//Nous allons maintenant dire à notre objet de se positionner au centre
-		this.setLocationRelativeTo(null);
+		this.setContentPane(new PanneauAdmin());
+		System.out.println("Affichage du menu de l'administrateur");
+		//Définit un titre pour notre fenêtre
+		this.setTitle("Menu de l'administrateur");
+		//Définit une taille pour celle-ci
+		this.setSize(new Dimension(700,500));		
+		this.setMinimumSize(new Dimension(700,500));
 		//Terminer le processus lorsqu'on clique sur "Fermer"
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Nous allons maintenant dire à notre objet de se positionner au centre
+		this.setLocationRelativeTo(null);
 
+		// on définit un BorderLayout
+		this.getContentPane().setLayout(new BorderLayout());
 		//pour que la fenêtre ne se redimensionne pas à chaque fois
 		this.setResizable(false);
 		//pour que la fenêtre soit toujours au premier plan
 		this.setAlwaysOnTop(true);
 
-		this.setContentPane(new PanneauAdmin());
-		this.setLayout(new BorderLayout());
+		this.setAdministrateur(a);
 
+			
 		labelAdmin = new JLabel("Vous êtes connecté en tant que "+ a.getCompte().getId());
-		labelAdmin.setFont(FenetreAuthentification.POLICE4);
+		labelAdmin.setFont(FenetreAuthentificationUtil.POLICE4);
+		labelAdmin.setPreferredSize(new Dimension(500,30));
+		boutonDeconnexion.setPreferredSize(new Dimension(150,30));
+		boutonDeconnexion.setBackground(Color.MAGENTA);
+		boutonDeconnexion.setFont(FenetreAuthentificationUtil.POLICE4);
 		boutonDeconnexion.addActionListener(this);
 		JPanel north = new JPanel();
+		north.setPreferredSize(new Dimension(700,150));
+		north.setBackground(FenetreAuthentificationUtil.TRANSPARENCE);
 		north.add(labelAdmin);
 		north.add(boutonDeconnexion);
 		this.getContentPane().add(north,BorderLayout.NORTH);
 
 		JPanel center = new JPanel();
-		boutonComptes.setPreferredSize(new Dimension(80,60));
+		center.setBackground(FenetreAuthentificationUtil.TRANSPARENCE);
+		boutonComptes.setPreferredSize(new Dimension(200,120));
+		boutonComptes.setMaximumSize(new Dimension(200,120));
+		boutonComptes.setFont(FenetreAuthentificationUtil.POLICE3);
 		boutonComptes.addActionListener(this);
 		center.add(boutonComptes);
-		boutonStats.setPreferredSize(new Dimension(80,60));
+		boutonStats.setPreferredSize(new Dimension(200,120));
+		boutonStats.setMaximumSize(new Dimension(200,120));
+		boutonStats.setFont(FenetreAuthentificationUtil.POLICE3);
 		boutonStats.addActionListener(this);
 		center.add(boutonStats);
-		boutonEtat.setPreferredSize(new Dimension(80,60));
+		boutonEtat.setPreferredSize(new Dimension(200,120));
+		boutonEtat.setMaximumSize(new Dimension(200,120));
+		boutonEtat.setFont(FenetreAuthentificationUtil.POLICE3);
 		boutonEtat.addActionListener(this);
 		center.add(boutonEtat);
 		this.add(center, BorderLayout.CENTER);
@@ -99,5 +117,10 @@ public class MenuPrincipalAdmin extends JFrame implements ActionListener {
 			FenetreConfirmation f = new FenetreConfirmation("Au revoir et à bientôt ! ");
 			f.setVisible(true);
 		}*/
+	}
+	
+	public static void main (String [] args){
+		Administrateur ATEST = new Administrateur(new Compte("administrateurTest","",Compte.TYPE_ADMINISTRATEUR));
+		new MenuPrincipalAdmin(ATEST);
 	}
 }
