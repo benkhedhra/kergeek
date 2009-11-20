5a7f4a9d73b32cc95f5f32b3df5af2413d59ece6
nicolas.martin747 <nicolas.martin747@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-11-20 12:45:42 +0000

    test DAO compte,admi,velo test ADMI,emprunt

---
+++ ProjetInfo/src/test/TestEmprunt.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestEmprunt extends TestCase {
+	@Test
+	public void testProposerDemanderIntervention(){
+		
+	}
+}
