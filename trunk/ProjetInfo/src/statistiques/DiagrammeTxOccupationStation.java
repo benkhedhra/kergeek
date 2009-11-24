3ce7eea077252e6791b8aec746708a6817ce74a1
berangerlueza@gmail.com <berangerlueza@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-11-24 16:07:45 +0000

    Station : constantes TAUX_OCCUP float->double DiagrammeTxOccup : le diagramme s'affiche, encore qqes bugs

--- trunk/ProjetInfo/src/statistiques/DiagrammeTxOccupationStation.java
+++ trunk/ProjetInfo/src/statistiques/DiagrammeTxOccupationStation.java
@@ -2,6 +2,7 @@ package statistiques;
 
 import java.awt.Image;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import metier.Station;
 
@@ -15,8 +16,6 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
-
-import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
@@ -44,7 +43,7 @@ public class DiagrammeTxOccupationStation extends ApplicationFrame {
  JFreeChart createChart(Station s) {
 
         // create subplot
-        final XYDataset data1 = createDataset();
+        final XYSeriesCollection data1 = createDataset();
         final XYItemRenderer renderer1 = new StandardXYItemRenderer();
         final NumberAxis rangeAxis1 = new NumberAxis("");
         final XYPlot subplot1 = new XYPlot(data1, null, rangeAxis1, renderer1);
@@ -67,38 +66,38 @@ public class DiagrammeTxOccupationStation extends ApplicationFrame {
     }
 
 
-    private XYDataset createDataset() {
-    	 
-    	
-    	//Changer de méthode cf site internet (marque pages)
-    	Calendar calendar = Calendar.getInstance(); 
-		int heureencours = calendar.get(Calendar.HOUR);
-		calendar.add(Calendar.HOUR, -1);
-		int heureent1 = calendar.get(Calendar.MONTH);
-		calendar.add(Calendar.HOUR, -1);
-		int heureent2 = calendar.get(Calendar.MONTH);
-		calendar.add(Calendar.HOUR, -1);
-		int heureent3 = calendar.get(Calendar.MONTH);
-		calendar.add(Calendar.HOUR, -1);
-		int heureent4 = calendar.get(Calendar.MONTH);
-    	
-        final XYSeries series1 = new XYSeries("Taux d'occupation");
-        series1.add(heureencours, 1);
-        series1.add(heureent1, 2);
-        series1.add(heureent2, 3);
-        series1.add(heureent3, 2.5);
-        series1.add(heureent4, 6);
+    private XYSeriesCollection createDataset() {
+    
+    	GregorianCalendar calendar = new GregorianCalendar(); 
+		int heureencours = calendar.get(Calendar.HOUR_OF_DAY);
+		calendar.add(Calendar.HOUR_OF_DAY, -1);
+		int heure3 = calendar.get(Calendar.HOUR_OF_DAY);
+		calendar.add(Calendar.HOUR_OF_DAY, -1);
+		int heure2 = calendar.get(Calendar.HOUR_OF_DAY);
+		calendar.add(Calendar.HOUR_OF_DAY, -1);
+		int heure1 = calendar.get(Calendar.HOUR_OF_DAY);
+		
+        final XYSeries series = new XYSeries("Taux d'occupation");
+        series.add(heureencours, 50.2);
+        series.add(heure3, 30.2);
+        series.add(heure2, 20.2);
+        series.add(heure1, 10.2);
+ 
        
         final XYSeries series2 = new XYSeries("Taux d'occupation minimum");
         series2.add(heureencours, Station.TAUX_OCCUPATION_MIN * 100);
-  
+        series2.add(heure3, Station.TAUX_OCCUPATION_MIN * 100);
+        series2.add(heure2, Station.TAUX_OCCUPATION_MIN * 100);
+        series2.add(heure1, Station.TAUX_OCCUPATION_MIN * 100);
         
         final XYSeries series3 = new XYSeries("Taux d'occupation maximum");
         series3.add(heureencours, Station.TAUX_OCCUPATION_MAX * 100);
-        
+        series3.add(heure3, Station.TAUX_OCCUPATION_MAX * 100);
+        series3.add(heure2, Station.TAUX_OCCUPATION_MAX * 100);
+        series3.add(heure1, Station.TAUX_OCCUPATION_MAX * 100);
 
         final XYSeriesCollection collection = new XYSeriesCollection();
-        collection.addSeries(series1);
+        collection.addSeries(series);
         collection.addSeries(series2);
         collection.addSeries(series3);
         return collection;
