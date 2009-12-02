747de9f354921d6c898ffdbb9d6f90946fa117e0
berangerlueza@gmail.com <berangerlueza@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-12-02 17:15:35 +0000

    tentative d'envoyer un mail: marche pas!!

--- trunk/ProjetInfo/src/envoieMail/EnvoieMail.java
+++ trunk/ProjetInfo/src/envoieMail/EnvoieMail.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -15,13 +16,13 @@ public class EnvoieMail {
 	 *Un truc que j'ai trouver sur internet pour envoyer des mails... a configurer, 
 	 *il faudrait nous creer une adresse kergeek (sur hotmail par exemple..)
 	 */
-	public void envoyerMail( String recipients[ ], String subject, String message , String from) throws MessagingException
+	public static void envoyerMail( String recipients, String subject, String message , String from) throws MessagingException
 	{
 		boolean debug = false;
 
 		//Set the host smtp address
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.jcom.net");
+		props.put("mail.smtp.host", "smtp.gmail.com");
 
 		// create some properties and get the default Session
 		Session session = Session.getDefaultInstance(props, null);
@@ -34,12 +35,15 @@ public class EnvoieMail {
 		InternetAddress addressFrom = new InternetAddress(from);
 		msg.setFrom(addressFrom);
 
-		InternetAddress[] addressTo = new InternetAddress[recipients.length]; 
+		/*InternetAddress[] addressTo = new InternetAddress[recipients.length]; 
 		for (int i = 0; i < recipients.length; i++)
 		{
 			addressTo[i] = new InternetAddress(recipients[i]);
 		}
-		msg.setRecipients(Message.RecipientType.TO, addressTo);
+		msg.setRecipients(Message.RecipientType.TO, addressTo);*/
+		
+		InternetAddress addressTo = new InternetAddress(recipients);
+		msg.setRecipient(null, addressTo);
 
 
 		// Optional : You can also set your custom headers in the Email if you Want
@@ -49,6 +53,11 @@ public class EnvoieMail {
 		msg.setSubject(subject);
 		msg.setContent(message, "text/plain");
 		Transport.send(msg);
+	}
+	
+	public static void main(final String[] args) throws MessagingException {
+		envoyerMail("kergeek@hotmail.fr", "test", "alors ca marche?", "berangerlueza@gmail.com");
+
 	}
 
 }
