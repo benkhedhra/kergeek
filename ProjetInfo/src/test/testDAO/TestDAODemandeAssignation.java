22de96157b7c923370a6481fd35e6d1ef08053e6
nicolas.martin747 <nicolas.martin747@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-12-03 11:02:11 +0000

    creaation deu test de la DAO dmdeAssignation j ai modifie date en dateAssignation dans creerAssign ainsi que un ' en trop dans la DAO emprunt

---
+++ ProjetInfo/src/test/testDAO/TestDAODemandeAssignation.java
@@ -0,0 +1,58 @@
+package test.testDAO;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+import gestionBaseDeDonnees.DAODemandeAssignation;
+import gestionBaseDeDonnees.DAOLieu;
+
+import metier.DemandeAssignation;
+import metier.Lieu;
+import metier.UtilitaireDate;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestDAODemandeAssignation extends TestCase{
+	/*@Test
+	public void testDemandeAssignation() throws SQLException, ClassNotFoundException{
+		Lieu l =DAOLieu.getLieuById("1");
+		Date d = UtilitaireDate.dateCourante();
+		DemandeAssignation dmde = new DemandeAssignation((java.sql.Date)d, 2, l);
+		Boolean b = DAODemandeAssignation.createDemandeAssignation(dmde);
+		assertEquals((Boolean)true,(Boolean) b);
+	}*/
+	
+
+	@Test
+	public void testGetDemandeAssignationById() throws SQLException, ClassNotFoundException{
+		DemandeAssignation dmde = DAODemandeAssignation.getDemandeAssignationById("1");
+		assertEquals("1", dmde.getLieu().getId());
+		assertEquals(5, dmde.getNombreVelosVoulusDansStation());
+		assertEquals("2009-11-23", dmde.getDate().toString());
+		assertEquals("1Gare du Campus", dmde.getLieu().toString());
+	}
+	
+	@Test
+	public void testUpdateDemandeAssignation() throws SQLException, ClassNotFoundException{
+		DemandeAssignation dmde = DAODemandeAssignation.getDemandeAssignationById("1");
+		dmde.setNombreVelosVoulusDansStation(7);
+		Boolean b = DAODemandeAssignation.updateDemandeAssignation(dmde);
+		assertEquals((Boolean) true, b);
+		DemandeAssignation dmde2 = DAODemandeAssignation.getDemandeAssignationById("1");
+		assertEquals(7, dmde2.getNombreVelosVoulusDansStation());
+		dmde2.setNombreVelosVoulusDansStation(5);
+		DAODemandeAssignation.updateDemandeAssignation(dmde2);
+	}
+	
+	@Test
+	public void testGetAllDemandesAssignation() throws SQLException, ClassNotFoundException{
+		List<DemandeAssignation> liste = DAODemandeAssignation.getAllDemandesAssignation();
+		assertEquals(5, liste.get(0).getNombreVelosVoulusDansStation());
+		assertEquals(4, liste.get(1).getNombreVelosVoulusDansStation());
+		assertEquals(10, liste.get(2).getNombreVelosVoulusDansStation());
+		assertEquals(4, liste.get(3).getNombreVelosVoulusDansStation());
+	}
+}
