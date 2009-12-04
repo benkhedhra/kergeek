0491ebebe4a2e6db468b5327edf65e6e58c033af
maelle.font@gmail.com <maelle.font@gmail.com@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-12-04 17:04:49 +0000

    création compte vraiment ok et utilitaire ihm mieux

--- trunk/ProjetInfo/src/ihm/UtilitaireIhm.java
+++ trunk/ProjetInfo/src/ihm/UtilitaireIhm.java
@@ -5,27 +5,20 @@ import exception.ChampIncorrectException;
 
 public class UtilitaireIhm {
 
-	public static boolean verifieChampsCreationCompte(int type,String adresseEMail) throws ChampIncorrectException{
-		boolean champsOk = false;
-		if ((type==1 || type==2 ||type==3) && (adresseEMail.length()>0 && (adresseEMail.length()<51))){
-			champsOk=true;
-		}
-		else {
-			throw new ChampIncorrectException("Champs entrés incorrects");
-		}
-		return champsOk;
+	public static boolean verifieTypeCreationCompte(int type){
+		return((type==1 || type==2 ||type==3));
+	}
+	
+	public static boolean verifieChampsCreationAdmin(int type,String adresseEMail){
+		return verifieTypeCreationCompte(type) && adresseEMail.length()>0 && adresseEMail.length()<51;
 	}
 
-	public static boolean verifieChampsCreationAdmin(Compte compte) throws ChampIncorrectException{
-		return verifieChampsCreationCompte(compte.getType(),compte.getAdresseEmail());
+	public static boolean verifieChampsCreationTech(int type,String adresseEMail){
+		return verifieTypeCreationCompte(type) && adresseEMail.length()>0 && adresseEMail.length()<51;
 	}
 
-	public static boolean verifieChampsCreationTech(Compte compte) throws ChampIncorrectException{
-		return verifieChampsCreationCompte(compte.getType(),compte.getAdresseEmail());
-	}
-
-	public static boolean verifieChampsCreationUtil(Compte compte,String nom,String prenom,String adressePostale) throws ChampIncorrectException{
-		return (verifieChampsCreationCompte(compte.getType(),compte.getAdresseEmail()) && prenom.length()>0 && prenom.length()<21 && nom.length()>0 && nom.length()<21 && adressePostale.length()>0 && adressePostale.length()<251);
+	public static boolean verifieChampsCreationUtil(int type,String adresseEMail,String nom,String prenom,String adressePostale){
+		return (verifieTypeCreationCompte(type) && adresseEMail.length()>0 && adresseEMail.length()<51 && prenom.length()>0 && prenom.length()<21 && nom.length()>0 && nom.length()<21 && adressePostale.length()>0 && adressePostale.length()<251);
 	}
 
 }
