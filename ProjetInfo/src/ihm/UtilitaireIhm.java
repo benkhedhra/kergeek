3b67aaee0317b8ccebea20128ab672484bef8baa
maelle.font <maelle.font@gmail.com>
2009-12-04 21:28:51 +0000

    normalement on ne peut modifier d'infos sur un compte que si les champs sont ok l'admin et le tech ont aussi la possibilité de changer leur mot de passe ds leur menu principal

--- ProjetInfo/src/ihm/UtilitaireIhm.java
+++ ProjetInfo/src/ihm/UtilitaireIhm.java
@@ -20,5 +20,8 @@ public class UtilitaireIhm {
 	public static boolean verifieChampsCreationUtil(int type,String adresseEMail,String nom,String prenom,String adressePostale){
 		return (verifieTypeCreationCompte(type) && adresseEMail.length()>0 && adresseEMail.length()<51 && prenom.length()>0 && prenom.length()<21 && nom.length()>0 && nom.length()<21 && adressePostale.length()>0 && adressePostale.length()<251);
 	}
-
+	
+	public static boolean verifieChampsModifMdp(Compte c,String ancienMdp,String nouveauMdp1,String nouveauMdp2){
+		return(c.getMotDePasse().equals(ancienMdp) && nouveauMdp1.length()>0 && nouveauMdp1.length()<21 && nouveauMdp1.equals(nouveauMdp2));
+	}
 }
