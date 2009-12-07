5ca8db5887e466f1aabd6b7a490f894cad7aa1b6
bhejblum <bhejblum@2757161c-bcf3-11de-a250-c7f789e21d83>
2009-12-07 13:00:02 +0000

    petit bug emprunt long reglé

--- trunk/ProjetInfo/src/ihm/appliUtil/FenetreEmpruntLong.java
+++ trunk/ProjetInfo/src/ihm/appliUtil/FenetreEmpruntLong.java
@@ -71,7 +71,7 @@ public class FenetreEmpruntLong extends JFrame implements ActionListener {
 		// l'utilisateur est bloqué pendant une semaine
 		utilisateur.setBloque(true);
 		labelMsg.setText("<html><center>Vous avez rendu le vélo plus de 2 heures après l'emprunt.<br>Votre compte sera bloqué pendant une semaine.<br>Remettez le vélo dans un emplacement.</center></html> ");
-		labelMsg.setPreferredSize(new Dimension(650,30));
+		labelMsg.setPreferredSize(new Dimension(340,120));
 		
 		JPanel center = new JPanel();
 		center.setBackground(FenetreAuthentificationUtil.TRANSPARENCE);
