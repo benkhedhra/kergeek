de6e398780ce034fa34d6bb965d56b1bc4765673
bhejblum <bhejblum@2757161c-bcf3-11de-a250-c7f789e21d83>
2010-01-05 12:55:25 +0000

    MonJTable non modifiable

--- trunk/ProjetInfo/src/statistiques/MonJTable.java
+++ trunk/ProjetInfo/src/statistiques/MonJTable.java
@@ -1,22 +1,38 @@
 package statistiques;
 
 import javax.swing.JTable;
-
+/**
+ * Cette classe permet de créer des tableaux non modifiables afin de présenter des données.
+ * @see JTable
+ * @author KerGeek
+ */
 public class MonJTable extends JTable {
 
 	private static final long serialVersionUID = 1L;
 
+	// empêche l'édition des cellules du tableaux
 	@Override
 	public boolean isCellEditable(int row, int column) {
 		return false;
 	}
-
+	
+	/**
+	 * Constructeur par défaut d'un MonJtable
+	 */
 	public MonJTable() {
 		super();
+		this.getTableHeader().setReorderingAllowed( false ); // empêche la modification de l'ordre des colonnes
 	}
-
+	
+	/**
+	 * TODO finir javadoc
+	 * Constructeur
+	 * @param rowData
+	 * @param columnNames
+	 */
 	public MonJTable(Object[][] rowData, Object[] columnNames) {
 		super(rowData, columnNames);
+		this.getTableHeader().setReorderingAllowed( false ); // empêche la modification de l'ordre des colonnes
 	}
 
 	
