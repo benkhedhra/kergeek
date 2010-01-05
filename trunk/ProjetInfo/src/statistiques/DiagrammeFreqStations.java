5c5c5d65588c15ef610a1a2edf6888ddad62d68d
bhejblum <bhejblum@2757161c-bcf3-11de-a250-c7f789e21d83>
2010-01-05 10:17:51 +0000

    graph freq station OK

--- trunk/ProjetInfo/src/statistiques/DiagrammeFreqStations.java
+++ trunk/ProjetInfo/src/statistiques/DiagrammeFreqStations.java
@@ -34,12 +34,12 @@ public class DiagrammeFreqStations extends ApplicationFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private JFreeChart chart;
 
 	public DiagrammeFreqStations(String periodeEntree) throws ConnexionFermeeException, SQLException, ClassNotFoundException {
 
-		super("Fréquentation des stations sur les "+periodeEntree);
+		super("Fréquentation des stations sur les " + periodeEntree);
 		CategoryDataset dataset = createDataset(periodeEntree);
 		chart = createChart(dataset,periodeEntree);
 		ChartPanel chartPanel = new ChartPanel(chart, false);
@@ -61,31 +61,31 @@ public class DiagrammeFreqStations extends ApplicationFrame {
 		String entres = "Vélos entrés";
 
 		// étiquettes des colonnes...
-			List<Station> stations = DAOLieu.getAllStations();
-			ArrayList<String> category = new ArrayList<String>(stations.size());
-			for (int i=0;i<stations.size();i++){
-				category.add(DAOLieu.getLieuById(stations.get(i).getId()).getAdresse());
-			}
+		List<Station> stations = DAOLieu.getAllStations();
+		ArrayList<String> category = new ArrayList<String>(stations.size());
+		for (int i=0;i<stations.size();i++){
+			category.add(DAOLieu.getLieuById(stations.get(i).getId()).getAdresse());
+		}
+		
+		// Création des données
+		int nbJoursEntre=365;
+		if (periodeEntree.equals("30 derniers jours")){
+			nbJoursEntre = 30;
+		}
+		else if (periodeEntree.equals("60 derniers jours")){
+			nbJoursEntre = 60;
+		}
+		else if (periodeEntree.equals("6 derniers mois")){
+			nbJoursEntre = 183;
+		}
+		else if (periodeEntree.equals("365 derniers jours")){
+			nbJoursEntre = 365;
+		}
+		for(int i=0;i<category.size();i++){
+			dataset.addValue(DAOEmprunt.NombreVelosRendusJours(stations.get(i), nbJoursEntre), entres, category.get(i));
+			dataset.addValue(DAOEmprunt.NombreVelosSortisJours(stations.get(i), nbJoursEntre), sortis, category.get(i));
 
-			int nbJoursEntre=365;
-			if (periodeEntree.equals("30 derniers jours")){
-				nbJoursEntre = 30;
-			}
-			else if (periodeEntree.equals("60 derniers jours")){
-				nbJoursEntre = 60;
-			}
-			else if (periodeEntree.equals("6 derniers mois")){
-				nbJoursEntre = 183;
-			}
-			else if (periodeEntree.equals("365 derniers jours")){
-				nbJoursEntre = 365;
-			}
-
-
-			for(int i=0;i<category.size();i++){
-				dataset.addValue(DAOEmprunt.NombreVelosSortisJours(stations.get(i), nbJoursEntre), sortis, category.get(i));
-				dataset.addValue(DAOEmprunt.NombreVelosRendusJours(stations.get(i), nbJoursEntre), entres, category.get(i));
-			}
+		}
 		return dataset;
 
 	}
@@ -108,35 +108,37 @@ public class DiagrammeFreqStations extends ApplicationFrame {
 		// couleur de l'arrière plan du chart
 		chart.setBackgroundPaint(Color.white);
 
-		// get a reference to the plot for further customisation...
+		// customisation de l'environnement du graphique
 		CategoryPlot plot = chart.getCategoryPlot();
 		plot.setBackgroundPaint(Color.lightGray);
 		plot.setDomainGridlinePaint(Color.white);
 		plot.setDomainGridlinesVisible(true);
 		plot.setRangeGridlinePaint(Color.white);
 
-		// set the range axis to display integers only...
+		// Réglage de l'axe des abscisses pour qu'il ne présente que des entiers
 		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
 		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
-		// disable bar outlines...
+		// Desactivation des bar outlines...
 		BarRenderer renderer = (BarRenderer) plot.getRenderer();
 		renderer.setDrawBarOutline(false);
 
-		// set up gradient paints for series...
+		// Définitions des couleurs des 2 series
 		GradientPaint gp0 = new GradientPaint(
-				0.0f, 0.0f, Color.blue, 
-				0.0f, 0.0f, new Color(0, 0, 64)
-		);
-		GradientPaint gp1 = new GradientPaint(
 				0.0f, 0.0f, Color.green, 
 				0.0f, 0.0f, new Color(0, 64, 0)
 		);
-
+		GradientPaint gp1 = new GradientPaint(
+				0.0f, 0.0f, Color.blue, 
+				0.0f, 0.0f, new Color(0, 0, 64)
+		);
 		renderer.setSeriesPaint(0, gp0);
 		renderer.setSeriesPaint(1, gp1);
 
+		
 		CategoryAxis domainAxis = plot.getDomainAxis();
+		
+		//Orientation de la légende
 		domainAxis.setCategoryLabelPositions(
 				CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
 		);
